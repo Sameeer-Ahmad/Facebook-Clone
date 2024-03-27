a7b373da9d2eb03dce4d0465a414eb022b8d85ee
SUJU1998 <111004060+SUJU1998@users.noreply.github.com>
2024-03-27 20:28:45 +0530

    day1

---
+++ facebook-clone/src/components/Story.tsx
@@ -0,0 +1,10 @@
+import { Heading } from '@chakra-ui/react';
+import React from 'react'
+
+export const Story=()=>{
+    return(
+        <>
+        <Heading>Story</Heading>
+        </>
+    );
+}
