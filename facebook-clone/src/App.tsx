8693e0c9465878d75ac350702b369ad67faa1ad4
sachinkumanache <kumanache@gmail.com>
2024-03-28 21:05:05 +0530

    sidebar 1

--- facebook-clone/src/App.tsx
+++ facebook-clone/src/App.tsx
@@ -3,12 +3,14 @@ import './App.css';
 
 import { Box } from "@chakra-ui/react";
 import Signup from "./pages/Signup";
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
   <div className='App'>
-   <Signup/>
-   </div>
+   {/* <Signup/> */}
+<Sidebar />
+    </div>
   );
 }
 
