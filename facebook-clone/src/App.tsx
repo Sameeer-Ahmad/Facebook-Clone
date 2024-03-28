04e435e016e643f67bb29b79b7349c9b2edfaca9
Sameeer-Ahmad <sam9910333@gmail.com>
2024-03-28 18:23:56 +0530

    signup-page_completed

--- facebook-clone/src/App.tsx
+++ facebook-clone/src/App.tsx
@@ -1,13 +1,12 @@
 
 import './App.css';
 
-import { Box } from "@chakra-ui/react";
-import Signup from "./pages/Signup";
+
 
 function App() {
   return (
   <div className='App'>
-   <Signup/>
+   
    </div>
   );
 }
