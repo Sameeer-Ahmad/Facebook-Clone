de2c913c95d60cec7c594caf6ab4c7c7c1e36050
Sameeer-Ahmad <sam9910333@gmail.com>
2024-04-03 13:46:24 +0530

    home page finised

--- facebook-clone/src/components/MiddleFeedParts/FeedSections/Story.tsx
+++ facebook-clone/src/components/MiddleFeedParts/FeedSections/Story.tsx
@@ -77,6 +77,7 @@ export const Story = () => {
               left={"-5px"}
               color={"white"}
             >
+              
               Sujeet Kumar
             </Text>
           </Box>
