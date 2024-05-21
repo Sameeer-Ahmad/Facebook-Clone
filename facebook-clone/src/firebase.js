0380b6f4399c838a8b72f021fbcff9d6710cd959
Sameeer-Ahmad <sam9910333@gmail.com>
2024-05-21 15:09:16 +0530

    make changes

--- facebook-clone/src/firebase.js
+++ facebook-clone/src/firebase.js
@@ -2,20 +2,28 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-
-//web app's Firebase configuration
+// web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD84SoyFYs4IH89CTJePXgyRYIfVnZ-70M",
-  authDomain: "facebook-clone-7ff4a.firebaseapp.com",
-  projectId: "facebook-clone-7ff4a",
-  storageBucket: "facebook-clone-7ff4a.appspot.com",
-  messagingSenderId: "539624084118",
-  appId: "1:539624084118:web:a9ba55e34a7427f56b28b7"
+  apiKey: "AIzaSyAsf0C7c8u1WkrW5euWG9L0bkMcgz9jdlw",
+  authDomain: "facebook-68002.firebaseapp.com",
+  databaseURL: "https://facebook-68002-default-rtdb.firebaseio.com",
+  projectId: "facebook-68002",
+  storageBucket: "facebook-68002.appspot.com",
+  messagingSenderId: "773582314334",
+  appId: "1:773582314334:web:6d0e7fd2107e945cb24dcf",
+  measurementId: "G-B49W1L8MVJ",
 };
-
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCRTan_zJC9UmGq4ulYLCJTDbDIVPrndrE",
+// //   authDomain: "facebook-clone-7ff4a.firebaseapp.com",
+//   databaseURL: "https://facebook-clone-7ff4a-default-rtdb.firebaseio.com",
+//   projectId: "facebook-clone-7ff4a",
+//   storageBucket: "facebook-clone-7ff4a.appspot.com",
+//   messagingSenderId: "539624084118",
+//   appId: "1:539624084118:web:7c016099f6898b086b28b7",
+// };
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-
 
 export const db = getFirestore(app);
 
